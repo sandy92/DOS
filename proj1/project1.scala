19e68222020f15149fd9d0aa19553353e816858e
Venkat Sandeep <sandeep.kv92@gmail.com>
2015-09-13 15:43:58 -0400

    proj1 - Implementing multiple Actors for mining

--- proj1/project1.scala
+++ proj1/project1.scala
@@ -6,14 +6,16 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util._
 
 case object InitializeSystem
-case object CreateSlave
-case object StartMining
-case object KillSystem
+case object KillActor
+case class AddSlave(s: ActorRef)
+case object SlaveAdded
+case object JobRequest
+case class Job(s: String, numZeros: Int)
+case class CoinFound(input: String, hash: String)
 
 object Hash {
-    val inst = md.getInstance("SHA-256")
     def sha256(s: String) = {
-        inst.digest(s.getBytes).map("%02x".format(_)).mkString
+        md.getInstance("SHA-256").digest(s.getBytes).map(e => "%02x".format(e)).mkString
     }
 }
 
@@ -21,10 +23,20 @@ class MiningSlaveActor extends Actor {
     var input = ""
     var hashValue = ""
     def receive = {
-        case msg: String =>
+        case SlaveAdded =>
+            sender ! JobRequest
+        case KillActor =>
+            context.stop(self)
+        case Job(msg: String, numZeros: Int) =>
             input = "kvsandeep;%s".format(msg)
             hashValue = Hash.sha256(input)
-            sender ! hashValue
+            if(this.checkZeros(hashValue, numZeros)) {
+                println(" Found")
+                sender ! CoinFound(input, hashValue)
+            } else {
+                sender ! JobRequest
+            }
+        case _ =>
     }
 
     def checkZeros(s: String, k: Int) = {
@@ -38,39 +50,35 @@ class MiningSlaveActor extends Actor {
 
 class MiningMasterActor extends Actor {
     val slaveActorList = ArrayBuffer.empty[ActorRef]
+    val numZeros = 5
     val queue = new Queue[String]
     var count = 0
-    //queue ++= List.range(0,1000000).map("%06d".format(_))
-    queue ++= List.range(0,1000).map("%03d".format(_))
 
     def receive = {
         case InitializeSystem =>
-            slaveActorList += context.system.actorOf(Props[MiningSlaveActor], name = "slave" + slaveActorList.length.toString)
-        case CreateSlave =>
-            // slaveActorList += context.system.actorOf(Props[MiningSlaveActor], name = "slave" + slaveActorList.length.toString)
-            var c = Future {
-                context.system.actorOf(Props[MiningSlaveActor], name = "slave" + slaveActorList.length.toString)
+            queue ++= List.range(0,1000000).map("%06d".format(_))
+        case AddSlave(s: ActorRef) =>
+            slaveActorList.append(s)
+            s ! SlaveAdded
+        case JobRequest =>
+            if(queue.length > 0) {
+                sender ! Job(queue.dequeue, numZeros)
+            } else {
+                println("Input stream exhausted")
+                context.system.shutdown
             }
-            c.onComplete {
-                case Success(actor) => slaveActorList += actor
-                case _ =>
+        case CoinFound(input: String, hash:String) =>
+            println(input + "\t" +hash)
+            self ! KillActor
+        case KillActor =>
+            while(slaveActorList.length > 0)
+            {
+                slaveActorList(0) ! KillActor
+                slaveActorList -= slaveActorList(0)
             }
-            println("slaveCreated")
-        case StartMining =>
-            var c = Future {
-                println("Mining Started")
-                while(queue.length > 0 ) {
-                    slaveActorList(count%slaveActorList.length) ! queue.dequeue
-                    count += 1
-                    //println("hey")
-                    println(slaveActorList.length)
-                }
-                println("Mining Ends")
-            }
-        case KillSystem =>
             context.system.shutdown
-        case msg: String =>
-            println(msg)
+        case "print" =>
+            println(slaveActorList)
         case _ =>
     }
 
@@ -79,11 +87,17 @@ class MiningMasterActor extends Actor {
 object Bitcoin extends App{
     val system = ActorSystem("BitcoinActorSystem")
     val master = system.actorOf(Props[MiningMasterActor], name="master")
+    val slave1 = system.actorOf(Props[MiningSlaveActor], name="slave1")
+    val slave2 = system.actorOf(Props[MiningSlaveActor], name="slave2")
+    val slave3 = system.actorOf(Props[MiningSlaveActor], name="slave3")
+    val slave4 = system.actorOf(Props[MiningSlaveActor], name="slave4")
+
     master ! InitializeSystem
-    master ! CreateSlave
-    master ! StartMining
-    //master ! CreateSlave
-    println("main here")
+
+    master ! AddSlave(slave1)
+    master ! AddSlave(slave2)
+    master ! AddSlave(slave3)
+    master ! AddSlave(slave4)
     /*
     if(args.length > 0 && false)
     {
@@ -105,7 +119,6 @@ object Bitcoin extends App{
     }
     */
     //system.shutdown
-
 
     def generateList(k:Int) = {
         if(k>0) {
